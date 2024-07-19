047c6e51c884bd41df1e70321763709b495c232a
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-07-19 20:32:35 -0300

    ptexer

---
+++ PythonBrasilExerc/01.py
@@ -0,0 +1,7 @@
+n1 = int(input("Escreva um número: "))
+n2 = int(input("Escreva um número: "))
+
+if (n1 > n2):
+    print(f"O número maior é o {n1}")
+else:
+    print(f"O número maior é o {n2}")
---
+++ PythonBrasilExerc/03.py
@@ -0,0 +1,8 @@
+letra = str(input("Escreva uma letra: "))
+
+if letra == "F":
+    print("Feminino")
+elif letra == "M":
+    print("Masculino")
+else:
+    print("Sexo inválido")
