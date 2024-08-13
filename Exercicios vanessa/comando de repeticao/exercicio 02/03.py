8c2b719d4e2c646f7a50bf485fe9fefaf0c8c4c7
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-08-12 21:16:11 -0300

    .3

--- Exercicios vanessa/comando de repeticao/exercicio 02/03.py
+++ Exercicios vanessa/comando de repeticao/exercicio 02/03.py
@@ -1,11 +1,7 @@
-# Escreva um programa que receba como entrada vários anos, até que seja informado um ano negativo, 
-# e exiba quantos anos lidos foram bissextos. (Obs: Há duas possibilidades para 
-# um ano ser bissexto: se ele for múltiplo de 400, ou se ele for múltiplo de 4 mas não for múltiplo de 100)
-
 ano = int(input("Escreva um ano: "))
 quantBissextos = 0
 while (ano >0) :
-    if (ano) or (ano // 400 == 0): 
+    if (ano % 400 == 0) or (ano % 4 == 0 and ano % 100 != 0): 
         quantBissextos += 1
     ano = int(input("Escreva outro ano: "))
 print(quantBissextos)
---
+++ Exercicios vanessa/comando de repeticao/exercicio 02/04.py
@@ -0,0 +1,8 @@
+quantVeiculos = int(input("Escreva a quantidade de veiculos: "))
+
+while quantVeiculos != 555:
+    if quantVeiculos >2:
+       valor = (quantVeiculos - 2) * 10
+    else:
+        valor = 0
+    quantVeiculos = int(input("Escreva a quantidade de veiculos: "))
