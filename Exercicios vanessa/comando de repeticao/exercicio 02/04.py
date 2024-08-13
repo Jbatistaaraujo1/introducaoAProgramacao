2c068bf15ddabad69b8b24479e2ff18045a0154e
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-08-12 23:20:35 -0300

    .4

--- Exercicios vanessa/comando de repeticao/exercicio 02/04.py
+++ Exercicios vanessa/comando de repeticao/exercicio 02/04.py
@@ -3,13 +3,11 @@ totalArrecadado = 0
 casa = 0
 while quantVeiculos != 555:
     if quantVeiculos >2:
-       multa = (quantVeiculos - 2) * 10
-       casa += 1       
+       multa = (quantVeiculos - 2) * 10    
        print(f"Multa casa: R$ {multa}")
        totalArrecadado += multa
     elif(quantVeiculos >0 and quantVeiculos <= 2):
         multa = 0
-        casa += 1
         print(f"Multa casa: R$ {multa}")
     quantVeiculos = int(input("Escreva a quantidade de veiculos: "))
 print(f"Total arrecadado: R$ {totalArrecadado}")
