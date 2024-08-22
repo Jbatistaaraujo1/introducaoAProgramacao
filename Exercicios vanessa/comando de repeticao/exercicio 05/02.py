58ab48c4bac6f46f278e1923a93a2fbb9516a10d
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-08-21 21:03:06 -0300

    .

--- Exercicios vanessa/comando de repeticao/exercicio 05/02.py
+++ Exercicios vanessa/comando de repeticao/exercicio 05/02.py
@@ -1,21 +1,21 @@
 continuar = "sim"
-total = 0
+totalArrecadado = 0
 formandos = 0
-pessoas = 0
+totalConvidados = 0
 while continuar != "não":
     quantConvi = int(input("Escreva a quantidade de convidados: "))
     if quantConvi > 15 and quantConvi < 25:
         valor = (quantConvi - 15) * 42
-        total += valor
-        pessoas += quantConvi
+        totalArrecadado += valor
+        totalConvidados += quantConvi
     elif quantConvi > 25:
         valor = 10 * 42
-        total += valor
-        pessoas += 25
+        totalArrecadado += valor
+        totalConvidados += 25
     else:
-        pessoas += quantConvi
+        totalConvidados += quantConvi
     formandos += 1
     continuar = str.lower(input("Deseja continuar? Digite sim ou não: "))
-pessoas += formandos
+totalConvidados += formandos
 print(f"{valor:.2f}")
-print(f"{pessoas}")
+print(f"{totalConvidados}")
