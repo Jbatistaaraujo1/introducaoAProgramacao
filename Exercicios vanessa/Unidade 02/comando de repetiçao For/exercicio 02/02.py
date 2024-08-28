cb36e3b7dbacb3e4dab43416e36bbdea6f8c7dac
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-08-27 22:55:06 -0300

    .

--- Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/02.py
+++ Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/02.py
@@ -1,20 +1,12 @@
-votoMarcilio = 0
-votoAurelio = 0
-votoBranco = 0
+votosMarcilio = 0
+votosAurelio = 0
+votosBranco = 0
 for cont in range(1,5,1):
-    voto = str.lower(input("Escreva o nome do candidato: "))
-    if (voto == "marcílio" ):
-        votoMarcilio += 1
-    elif(voto == "aurelio"):
-        votoAurelio += 1
-    else:
-        votoBranco += 1
-if votoMarcilio > votoAurelio and votoMarcilio > votoBranco:
-    print("Marcílio")
-elif votoAurelio > votoMarcilio and votoAurelio > votoBranco:
-    print("Aurélio")
-elif votoBranco >= votoMarcilio and votoBranco >= votoAurelio:
-    print("Nova votação")
-else:
-    print("Nova votação")
-############
+    voto = str.lower(input("Escreva o seu voto: "))
+    if voto == "marcílio":
+        votosMarcilio += 1
+    elif voto == "aurélio":
+        votosAurelio += 1
+    elif voto == "branco":
+        votosBranco += 1
+if 
