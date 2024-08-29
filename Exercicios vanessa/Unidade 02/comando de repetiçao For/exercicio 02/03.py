241ee988422d5350fcc03e1d361e8e15280c3541
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-08-29 08:53:24 -0300

    .

--- Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/03.py
+++ Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/03.py
@@ -1,6 +1,8 @@
 quantMultiplos = 0
-for cont in range(1,3,1):
-    numero = int(input("Escreva um número: "))
-    if numero % 4 == 0:
-        quantMultiplos += 1
+numero1 = int(input("Escreva um número: "))
+numero2 = int(input("Escreva um número: "))
+if numero1 > numero2:
+    for cont in range(numero1 + 1,numero2,1):
+        if cont % 4 == 0:
+            quantMultiplos += 1
 print(f"{quantMultiplos} múltiplos")
