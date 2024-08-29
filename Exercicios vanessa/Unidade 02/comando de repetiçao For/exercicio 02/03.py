0b02c7656e08ae7ca92c5c51a6cdc07dbccb1c51
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-08-29 09:08:24 -0300

    .

--- Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/03.py
+++ Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/03.py
@@ -1,8 +1,11 @@
 quantMultiplos = 0
 numero1 = int(input("Escreva um número: "))
 numero2 = int(input("Escreva um número: "))
-if numero1 > numero2:
-    for cont in range(numero1 + 1,numero2,1):
-        if cont % 4 == 0:
-            quantMultiplos += 1
+if (numero1 > numero2):
+    temporaria = numero1
+    numero1 = numero2
+    numero2 = temporaria
+for cont in range(numero1 + 1,numero2,1):
+    if cont % 4 == 0:
+        quantMultiplos += 1
 print(f"{quantMultiplos} múltiplos")
--- Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/05.py
+++ Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/05.py
@@ -1,6 +1,6 @@
 salario = int(input("Digite seu salario: "))
 total = 0
-for cont in range(1,13,1):
+for cont in range(1,4,1):
     despesas = int(input("Escreva o valor das despesas: "))
     Economizado = salario - despesas
     total += Economizado
