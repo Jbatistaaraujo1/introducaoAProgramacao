552768e8958bc5ddfeab026d8b6510b053be8b30
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-09-02 21:26:11 -0300

    .

--- Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/06.py
+++ Exercicios vanessa/Unidade 02/comando de repetiçao For/exercicio 02/06.py
@@ -1,6 +1,12 @@
+qtde = 0
+notaAnterior = 0
 for cont in range(1,7,1):
     nota = float(input("Escreva a nota:"))
-    
-    
-
-##
+    if nota > notaAnterior :
+        melhorNota = nota
+        qtde += 1
+    notaAnterior = nota
+if qtde == 6:
+    print("Ganha Brinquedo!")
+else:
+    print("Não Ganha Brinquedo!")
