b0aca676be35f5a6a01376e01ae3068f4650b488
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-09-06 00:08:19 -0300

    .

--- Exercicios vanessa/Unidade 02/listas/exercicio 02/02.py
+++ Exercicios vanessa/Unidade 02/listas/exercicio 02/02.py
@@ -2,11 +2,11 @@ import random
 
 quantNumeros = 10
 lista = []
+for i in range(quantNumeros):
+    numero = random.randint(1,100)
+    lista.append(numero)
 
-numero = [random.randint(1,100) for i in range(quantNumeros)]
-lista.append(numero)
-
-menorNumero = 0
+menorNumero = lista[0]
 quantImpar = 0
 for i in range(quantNumeros):
     if lista[i] < menorNumero:
@@ -14,8 +14,10 @@ for i in range(quantNumeros):
     if lista[i] % 2 != 0:
         quantImpar += 1
 soma = lista[0]+lista[1]+lista[2]+lista[3]+lista[4]
-
-print(lista)
+print()
+print(f"A lista gerada foi: {lista}")
+print()
+print()
 print(f"Primeiro elemento: {lista[0]} , Último elemento: {lista[9]}")
 print(f"A soma dos 5 primeiros elementos: {soma}")
 print(f"O menor elemento: {menorNumero}")
