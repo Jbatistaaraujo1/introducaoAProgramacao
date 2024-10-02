4d246f28d66f3da0c672b5aa15de1862fbdff9b1
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-10-01 22:47:20 -0300

    01

---
+++ Exercicios vanessa/Unidade 03/Funções e bibliotecas/Lista 03/01.py
@@ -0,0 +1,15 @@
+import bibGaleriaArte
+
+obras = []
+for i in range(60):
+    nomes = str.lower(input("Digite o nome da obra: "))
+    obras.append(nomes)
+valorTotal = 0
+for i in range(len(obras)):
+    if bibGaleriaArte.consultaArtista(obras[i]).lower() == "leonardo resende":
+        valorTotal += bibGaleriaArte.consultaPreco(obras[i])
+        
+if valorTotal == 0:
+    print("Não há Obras desse artista")
+else:
+    print(f"O valor total das obras de Leonardo Resende foi: R$ {valorTotal}")
