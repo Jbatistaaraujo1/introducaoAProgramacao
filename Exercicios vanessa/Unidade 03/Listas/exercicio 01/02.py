aa7f6167ee7cff8b2ceb967d882e00679dc0b159
Jbatistaaraujo1 <joaobatistaaraujosouto@gmail.com>
2024-10-10 22:12:38 -0300

    02

--- Exercicios vanessa/Unidade 03/Listas/exercicio 01/02.py
+++ Exercicios vanessa/Unidade 03/Listas/exercicio 01/02.py
@@ -1,6 +1,9 @@
 import bibliotecalistas
 
-numeros = [int(input())]
-
-print(bibliotecalistas.multiplicaLista(numeros,2))
-print(numeros)
+numeros = int(input("Escreva a quantidade de numeros da lista: "))
+valor = int(input("Escreva o valor que vai multiplicar a lista: "))
+lista = []
+for i in range(numeros):
+    num = int(input(f"Escreva o {i}º número: "))
+    lista.append(num)
+print(bibliotecalistas.multiplicaLista(lista, valor))
